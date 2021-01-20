0eab5249ee0efd0b95a61c9024a5cb68abe7f239
juanmalob <juanmalobato@gmail.com>
2021-01-20 06:04:23 +0100

    Añadir about

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
 
   root 'pages#home' ##lo envia a application controller. controller#metodo
 
+  get 'about', to: 'pages#about' #lo mandamos a controller de pages.
+
 end
